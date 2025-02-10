a9a3ee68a8c2208ccf2193b728b6536490f46811
Saad Edroos <saadedroossaad@gmail.com>
2025-02-10 11:59:09 +0400

    Change links

--- data/footer.js
+++ data/footer.js
@@ -40,7 +40,7 @@ export const currencyOptions = [
   { value: "qa", text: "QATAR", selected: true, link: "https://qa.ahmedalmaghribi.com" },
   { value: "bh", text: "BAHRAIN", link: "https://bh.ahmedalmaghribi.com" },
   { value: "ksa", text: "KSA",link: "https://ksa.ahmedalmaghribi.com" },
-  { value: "/", text: "UAE", link: "https://www.ahmed-perfume.com" },
+  { value: "/", text: "UAE", link: "https://ae.ahmedalmaghribi.com" },
   { value: "om", text: "OMAN", link: "https://om.ahmedalmaghribi.com" },
   { value: "kw", text: "KUWAIT", link: "https://kw.ahmedalmaghribi.com" },
 ];

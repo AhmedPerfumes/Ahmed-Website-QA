28af8df09294b52d2f93800eb94953dd912b7a3a
Yazil Ansari <yazilansari@gmail.com>
2025-03-13 14:07:13 +0400

    Added Payment Gateway

--- components/shopCartandCheckout/Checkout.jsx
+++ components/shopCartandCheckout/Checkout.jsx
@@ -213,7 +213,72 @@ export default function Checkout() {
         setSuccess(data.message);
         setError(null);
         // localStorage.setItem('orderData', btoa(JSON.stringify(data)));
-        router.push(data.redirect_url);
+        // router.push(data.redirect_url);
+        const form = document.createElement("form");
+        form.action = data.redirect_url;  // The URL to redirect to
+        form.method = "POST";
+
+        // If you have additional data you want to send with the form
+        const params = {
+          // Add parameters you need to pass to the form (hidden inputs)
+          'access_key': data.access_key,
+          'profile_id': data.profile_id,
+          'transaction_uuid': data.transaction_uuid,
+          'signed_field_names': data.signed_field_names,
+          'unsigned_field_names': data.unsigned_field_names,
+          'signed_date_time': data.signed_date_time,
+          'locale': data.locale,
+          'transaction_type': data.transaction_type,
+          'reference_number': data.order_id,
+          'amount': data.total,
+          'currency': data.currency,
+          'bill_to_forename': data.bill_to_forename,
+          'bill_to_surname': data.bill_to_surname,
+          'bill_to_email': data.bill_to_email,
+          'bill_to_phone': data.bill_to_phone,
+          'bill_to_address_line1': data.bill_to_address_line1,
+          'bill_to_address_city': data.bill_to_address_city,
+          'bill_to_address_state': data.bill_to_address_state,
+          'bill_to_address_country': data.bill_to_address_country,
+          'ship_to_forename': data.ship_to_forename,
+          'ship_to_surname': data.ship_to_surname,
+          'ship_to_email': data.ship_to_email,
+          'ship_to_phone': data.ship_to_phone,
+          'ship_to_address_line1': data.ship_to_address_line1,
+          'ship_to_address_city': data.ship_to_address_city,
+          'ship_to_address_state': data.ship_to_address_state,
+          'ship_to_address_country': data.ship_to_address_country,
+          'submit': 'Submit',
+          'signature': data.signature
+        };
+
+        // console.log(JSON.stringify(params));return;
+
+        Object.entries(params).forEach(([key, value]) => {
+          const input = document.createElement("input");
+          input.type = "hidden";
+          input.name = key;
+          input.value = value;
+          form.appendChild(input);
+        });
+
+        const submitButton = document.createElement('input');
+        submitButton.type = 'submit';
+        // submitButton.style.display = 'none';  // Hide the button
+        submitButton.value = 'Confirm';
+
+        form.appendChild(submitButton);
+
+        // Appending the form to the body
+        document.body.appendChild(form);
+
+        // Submit the form
+        // form.submit();return;
+        console.log('Form is valid:', form);
+
+        // Trigger the button click
+        setTimeout(() => submitButton.click(), 1000);
+        // submitButton.click();return;
       } else {
         if(data.products) {
           setError(data.products);
@@ -906,29 +971,41 @@ export default function Checkout() {
                     </span> */}
                   </label>
                 </div>
-                {/* <div className="form-check">
+                <div className="form-check">
                   <input
                     className="form-check-input form-check-input_fill"
                     type="radio"
                     name="checkout_payment_method"
                     id="checkout_payment_method_4"
-                    value={'paytabs'}
-                    checked={selectedOption === 'paytabs'}
+                    value={'cybersource'}
+                    checked={selectedOption === 'cybersource'}
                     onChange={handleRadioChange}
                   />
                   <label
                     className="form-check-label"
                     htmlFor="checkout_payment_method_4"
                   >
-                    PayTabs - Credit / Debit Card
-                    <Image
+                    CyberSource - Credit / Debit Card
+                      <svg xmlns="http://www.w3.org/2000/svg" width="60" height="20" viewBox="0 0 77 16">
+                        <g transform="translate(-523 -415)">
+                            <rect style={{fill: "#fff", opacity: 0}} class="a" width="77" height="16" transform="translate(523 415)"/>
+                            <path style={{fill: "#2a2a6c"}} class="b" d="M70.75,432.369l-5.76,13.746H61.23L58.4,435.145a1.522,1.522,0,0,0-.847-1.21,15.018,15.018,0,0,0-3.509-1.167l.087-.4h6.049a1.657,1.657,0,0,1,1.64,1.4l1.5,7.955,3.7-9.357H70.75m14.727,9.256c.017-3.625-5.017-3.823-4.98-5.446.009-.494.479-1.019,1.507-1.151a6.719,6.719,0,0,1,3.507.612l.624-2.912a9.55,9.55,0,0,0-3.325-.609c-3.515,0-5.989,1.869-6.009,4.543-.023,1.978,1.765,3.082,3.113,3.741,1.385.674,1.847,1.1,1.842,1.708-.008.923-1.1,1.325-2.126,1.344a7.433,7.433,0,0,1-3.654-.869l-.644,3.014a10.87,10.87,0,0,0,3.956.731c3.735,0,6.178-1.849,6.19-4.707m9.28,4.49h3.29l-2.87-13.746H92.14a1.624,1.624,0,0,0-1.514,1.007l-5.332,12.739h3.732l.741-2.053H94.33Zm-3.967-4.87,1.872-5.161,1.077,5.161Zm-14.959-8.875L72.89,446.114H69.334l2.942-13.746Z" transform="translate(470.495 -16.119)"/>
+                            <g transform="translate(1.466 -18.353)">
+                            <rect style={{fill: "#ff5f00"}} class="c" width="6.84" height="11.172" transform="translate(581.019 435.873)"/>
+                            <path style={{fill: "#eb001b"}} class="d" d="M16.226,14.558A7.093,7.093,0,0,1,18.94,8.973a7.1,7.1,0,1,0,0,11.172,7.093,7.093,0,0,1-2.714-5.587Z" transform="translate(565.497 426.902)"/>
+                            <path style={{fill: "#f79e1b"}} class="e" d="M119.946,64.636v-.229h.1V64.36h-.235v.047h.093v.229Zm.456,0V64.36h-.071l-.083.2-.083-.2h-.071v.276h.051v-.209l.077.18h.053l.077-.18v.209Z" transform="translate(475.307 381.226)"/>
+                            <path style={{fill: "#f79e1b"}} class="e" d="M77.186,14.547a7.1,7.1,0,0,1-11.5,5.585,7.1,7.1,0,0,0,0-11.172,7.1,7.1,0,0,1,11.5,5.585Z" transform="translate(518.747 426.913)"/>
+                            </g>
+                        </g>
+                      </svg>
+                    {/* <Image
                       src="https://www.ahmedalmaghribi.com/wp-content/plugins/paytabs-woocommerce/icons/creditcard.svg"
                       width="100"
                       height="20"
                       alt="Cropped Faux leather Jacket"
-                    />
+                    /> */}
                   </label>
-                </div> */}
+                </div>
                 <div className="policy-text">
                   Your personal data will be used to process your order, support
                   your experience throughout this website, and for other

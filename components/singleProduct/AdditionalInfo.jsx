0af69ef8dd6cf27967374f2d037d7e77afe489db
Saad Edroos <saadedroossaad@gmail.com>
2025-03-28 10:46:53 +0400

    Remove unnecessary text for seo

--- components/singleProduct/AdditionalInfo.jsx
+++ components/singleProduct/AdditionalInfo.jsx
@@ -24,74 +24,7 @@ export default function AdditionalInfo({ product_name, video, title }) {
   }
   return (
     <>
-      {/* <div className="d-lg-flex align-items-lg-center mb-5">
-        <div className="image-wrapper col-lg-6">
-          <img
-            style={{ height: "fit-content" }}
-            className="h-auto"
-            loading="lazy"
-            src="https://www.ahmedalmaghribi.com/wp-content/uploads/2024/03/Top-Notes-1.jpg"
-            alt="image"
-          />
-        </div>
-        <div className="content-wrapper col-lg-6 px-lg-4">
-          <h3 className="mb-3 text-white">The Company</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sapien
-            dignissim a elementum. Sociis metus, hendrerit mauris id in. Quis
-            sit sit ultrices tincidunt euismod luctus diam. Turpis sodales orci
-            etiam phasellus lacus id leo. Amet turpis nunc, nulla massa est
-            viverra interdum. Praesent auctor nulla morbi non posuere mattis.
-            Arcu eu id maecenas cras.
-          </p>
-        </div>
-      </div>
-
-      <div className="d-lg-flex align-items-lg-center mb-5">
-        <div className="content-wrapper col-lg-6 px-lg-4">
-          <h3 className="mb-3 text-white">The Company</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sapien
-            dignissim a elementum. Sociis metus, hendrerit mauris id in. Quis
-            sit sit ultrices tincidunt euismod luctus diam. Turpis sodales orci
-            etiam phasellus lacus id leo. Amet turpis nunc, nulla massa est
-            viverra interdum. Praesent auctor nulla morbi non posuere mattis.
-            Arcu eu id maecenas cras.
-          </p>
-        </div>
-        <div className="image-wrapper col-lg-6">
-          <img
-            style={{ height: "fit-content" }}
-            className="h-auto"
-            loading="lazy"
-            src="https://www.ahmedalmaghribi.com/wp-content/uploads/2024/03/middle-notes.jpg"
-            alt="image"
-          />
-        </div>
-      </div>
-
-      <div className="d-lg-flex align-items-lg-center mb-5">
-        <div className="image-wrapper col-lg-6">
-          <img
-            style={{ height: "fit-content" }}
-            className="h-auto"
-            loading="lazy"
-            src="https://www.ahmedalmaghribi.com/wp-content/uploads/2024/03/Base-Notes.jpg"
-            alt="image"
-          />
-        </div>
-        <div className="content-wrapper col-lg-6 px-lg-4">
-          <h3 className="mb-3 text-white">The Company</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sapien
-            dignissim a elementum. Sociis metus, hendrerit mauris id in. Quis
-            sit sit ultrices tincidunt euismod luctus diam. Turpis sodales orci
-            etiam phasellus lacus id leo. Amet turpis nunc, nulla massa est
-            viverra interdum. Praesent auctor nulla morbi non posuere mattis.
-            Arcu eu id maecenas cras.
-          </p>
-        </div>
-      </div> */}
+      
       <div dangerouslySetInnerHTML={{ __html: t.raw(cleanProductName(product_name)) }}></div>
       <div className="align-items-lg-center mb-5">
         <div className="col-lg-12 d-flex align-items-center justify-content-center mb-5">

82ec88b8c37275c352ed8f3ff565ed860a79abf3
Saad Edroos <saadedroossaad@gmail.com>
2025-06-16 11:53:31 +0400

    Remove gift

--- components/shopCartandCheckout/Checkout.jsx
+++ components/shopCartandCheckout/Checkout.jsx
@@ -20,7 +20,7 @@ import { products1 } from "@/data/products/fashion";
 import { useRouter } from 'next/navigation';
 import { useLocale } from "next-intl";
 import Pagination1 from "../common/Pagination1";
-import FreeGiftFeature from '@/components/FreeGiftFeature';
+// import FreeGiftFeature from '@/components/FreeGiftFeature';
 
 export default function Checkout() {
   const { shippingServiceCharges, vatTax, isLoading: isMenuLoading, error: isMenuError, currency } = useMenu();
@@ -571,7 +571,7 @@ export default function Checkout() {
 
   return (
     <>
-    <FreeGiftFeature couponData={couponData}/>
+    {/* <FreeGiftFeature couponData={couponData}/> */}
     {cartProducts.length ? (
       <form onSubmit={onOrder}>
         <div className="checkout-form">

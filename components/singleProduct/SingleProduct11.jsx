59714b9454cca09bc2653dd4fef976df582643f0
Saad Edroos <saadedroossaad@gmail.com>
2025-07-05 14:51:15 +0400

    Added product description

--- components/singleProduct/SingleProduct11.jsx
+++ components/singleProduct/SingleProduct11.jsx
@@ -215,7 +215,13 @@ export default function SingleProduct11({ category, subcategory, product }) {
             <Description product_name={ product.product_name }/>
           </div>
           <h2 className="product-single__details-list__title text-white">
-           {category === "gift-sets" ? "Gift Set Contains" : "Fragrance Notes"}
+          {category === "gift-sets"
+  ? "Gift Set Contains"
+  : category === "collections"
+  ? "Bundle Consist of"
+  : "Fragrance Notes"}
+
+
           </h2>
           <div className="product-single__details-list__content text-white">
             <AdditionalInfo product_name={ product.product_name } video={ product.video && JSON.parse(product.video)[0][0].value } title={ product.video[0][1] && JSON.parse(product.video)[0][1].value }/>

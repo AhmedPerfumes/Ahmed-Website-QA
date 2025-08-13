4f5e95c2518994da3857b7ae3508ea19e5bb58b4
Yazil Ansari <yazilansari@gmail.com>
2025-08-13 17:49:41 +0400

    Resolved Conflict

--- components/shopCartandCheckout/OrderPaymentCompleted.jsx
+++ components/shopCartandCheckout/OrderPaymentCompleted.jsx
@@ -38,13 +38,13 @@ export default function OrderPaymentCompleted({ orderDetails }) {
         console.log('else if');
         return <td>{((elm.price - (elm.price / 100 * elm.coupon.value)) * elm.quantity).toFixed(2)}{ currency.symbol }</td>;
     } else if(elm?.sale_price) {
-        return <td>{(((elm.price * 1.15) - ((elm.price * 1.15) / 100 * elm.sale_price)) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
+        return <td>{(((elm.price * 1) - ((elm.price * 1) / 100 * elm.sale_price)) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
     } else {
       console.log('else');
       if(elm?.product_category && elm.product_category == 'Collections') {
         return <td>{ elm.gross_amount }{ currency.symbol }</td>;
       }
-      return <td>{((elm.price * 1.05) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
+      return <td>{((elm.price * 1) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
   }
   };
 

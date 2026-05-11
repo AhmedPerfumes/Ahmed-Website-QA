aed8a3772ef5b21b383414a7eb10bd2ec71e9745
saood ghare <saoodghare0809@gmail.com>
2026-05-11 09:28:03 +0400

    order tracking added hash

--- components/shopCartandCheckout/OrderTrack.jsx
+++ components/shopCartandCheckout/OrderTrack.jsx
@@ -12,6 +12,7 @@ export default function OrderTrack() {
   const [success, setSuccess] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
   const [orderDetails, setOrderDetails] = useState({});
+  const [orderNumber, setOrderNumber] = useState("#");
 
   if (isMenuLoading) {
     return <div><Pagination1 /></div>;
@@ -114,8 +115,17 @@ export default function OrderTrack() {
               className="form-control"
               id="order_tracking_id"
               placeholder="Order ID *"
+              value={orderNumber}
               name="order_number"
               required
+              onChange={(e) => {
+                const input = e.target.value;
+                if (!input.startsWith("#")) {
+                  setOrderNumber("#" + input.replaceAll("#", ""));
+                } else {
+                  setOrderNumber(input);
+                }
+              }}
             />
             <label htmlFor="order_tracking_id">Order ID *</label>
           </div>

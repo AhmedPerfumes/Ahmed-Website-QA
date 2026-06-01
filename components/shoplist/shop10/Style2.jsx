aa98069997c53ba4b480482b527ff066d7ef6627
Saad Edroos <saadedroossaad@gmail.com>
2026-06-01 14:32:47 +0400

    Fixed sale badge

--- components/shoplist/shop10/Style2.jsx
+++ components/shoplist/shop10/Style2.jsx
@@ -235,7 +235,7 @@ console.log(currency, "currency");
                       ) : (
                         elm.discount && (
                           <div style={{ backgroundColor: '#198754' }} className="product-label text-uppercase text-white top-0 left-0 mt-2 mx-2">
-                            Sale {elm.discount.value}%
+                            {elm.discount.discount_type === "percent" ? `Sale ${elm.discount.value}%` : "Sale"}
                           </div>
                         )
                       )}

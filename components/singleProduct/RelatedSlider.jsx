a712ec6c9f7bdb0d8e3305a431f46a356341eaef
Saad Edroos <saadedroossaad@gmail.com>
2026-06-23 14:26:15 +0400

    fixed price

--- components/singleProduct/RelatedSlider.jsx
+++ components/singleProduct/RelatedSlider.jsx
@@ -191,8 +191,8 @@ export default function RelatedSlider({ relatedProds }) {
                 ) : (
                   elm.discount && (
                     <div style={{ backgroundColor: '#198754' }} className="product-label text-uppercase text-white top-0 left-0 mt-2 mx-2">
-                      Sale {elm.discount.value}%
-                    </div>
+                            {elm.discount.discount_type === "percent" ? `Sale ${elm.discount.value}%` : "Sale"}
+                          </div>
                   )
                 )}
                 {
